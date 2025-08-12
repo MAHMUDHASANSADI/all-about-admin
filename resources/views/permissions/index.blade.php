e20b8bc8d864299f451e900ad0b6b1f3a627bef3
MAHMUDHASANSADI <mhasan181075@bscse.uiu.ac.bd>
2025-08-12 17:12:00 +0600

    design update

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -11,7 +11,9 @@
     </x-slot>
 
     <div class="py-12">
-        @include('components.message')
+        <div class="max-w-lg mx-auto">
+            @include('components.message')
+        </div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class=" p-6 text-gray-900 dark:text-gray-100">
